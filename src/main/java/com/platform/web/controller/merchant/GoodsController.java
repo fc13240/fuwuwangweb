7d4a181f51fa2ddcd7c2b75e5d1fb334e1429b9c
LeeJetty <LeeJetty@Lee>
2016-01-11 15:19:44 +0800

    修改商品信息，商品状态改为待审核状态

--- src/main/java/com/platform/web/controller/merchant/GoodsController.java
+++ src/main/java/com/platform/web/controller/merchant/GoodsController.java
@@ -166,7 +166,7 @@ public class GoodsController extends BaseController {
 			String goods_purchase_notes, HttpSession session) throws Exception {
 		Goods goods = new Goods();
 		goods.setGoods_id(goods_id);
-		goods.setGoods_check_state(Constants.GOODS_ACTIVE);
+		goods.setGoods_check_state(Constants.GOODS_WAIT);
 		goods.setGoods_delete_state(Constants.GOODS_NORMAL);
 		goods.setGoods_type2_id(1);
 
